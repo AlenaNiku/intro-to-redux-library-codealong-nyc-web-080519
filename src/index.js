2d91fded76635644f3b9009acb819a66f824084c
Alena Nikulina <alenanikulina0@gmail.com>
2019-12-04 20:39:39 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'; /* code change */
+import { Provider } from 'react-redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(shoppingListItemReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+     ,document.getElementById('root')
+);
+
+
+// We want to import createStore() in our src/index.js file, where ReactDOM renders our application.
+
+// To avoid passing store as a prop, we use the Provider component, which is imported from react-redux. The Provider component wraps the top level component, App, in this case, and is the only component where store is passed in
